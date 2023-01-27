f0bd23b1b594655958ae9bf513bebd94b2720991
Will Crichton <wcrichto@cs.stanford.edu>
2023-01-27 02:23:44 -0800

    Add caching to mdbook-aquascope (#20)
    * Add caching to mdbook-aquascope
    
    * Install aquascope_front for mdbook-aquascope tests

---
+++ crates/mdbook-aquascope/src/cache.rs
@@ -0,0 +1,74 @@
+use std::{
+  collections::{hash_map::DefaultHasher, HashMap},
+  fs::{File, OpenOptions},
+  hash::{Hash, Hasher},
+  io::{Seek, Write},
+  marker::PhantomData,
+};
+
+use anyhow::Result;
+use flate2::{read::GzDecoder, write::GzEncoder, Compression};
+use serde::{de::DeserializeOwned, Serialize};
+
+pub struct Cache<K, V> {
+  cache_file: File,
+  cache: HashMap<u64, V>,
+  dirty: bool,
+  _key: PhantomData<K>,
+}
+
+pub const CACHE_PATH: &str = ".aquascope-cache";
+
+fn hash<K: Hash>(k: &K) -> u64 {
+  let mut hasher = DefaultHasher::new();
+  k.hash(&mut hasher);
+  hasher.finish()
+}
+
+impl<K: Hash, V: Serialize + DeserializeOwned> Cache<K, V> {
+  pub fn load() -> Result<Self> {
+    let cache_file = OpenOptions::new()
+      .read(true)
+      .write(true)
+      .create(true)
+      .open(CACHE_PATH)?;
+    let cache = if cache_file.metadata()?.len() == 0 {
+      HashMap::new()
+    } else {
+      let mut decoder = GzDecoder::new(&cache_file);
+      serde_json::from_reader(&mut decoder).unwrap_or_else(|e| {
+        eprintln!("Warning: failed to read Aquascope cache with error {e}");
+        HashMap::new()
+      })
+    };
+    Ok(Cache {
+      cache,
+      cache_file,
+      dirty: false,
+      _key: PhantomData,
+    })
+  }
+
+  pub fn get(&self, key: &K) -> Option<&V> {
+    self.cache.get(&hash(key))
+  }
+
+  pub fn set(&mut self, key: K, value: V) {
+    self.cache.insert(hash(&key), value);
+    self.dirty = true;
+  }
+
+  pub fn save(&mut self) -> Result<()> {
+    if self.dirty {
+      self.cache_file.set_len(0)?;
+      self.cache_file.rewind()?;
+      let mut encoder =
+        GzEncoder::new(&mut self.cache_file, Compression::best());
+      serde_json::to_writer(&mut encoder, &self.cache)?;
+      encoder.finish()?;
+      self.cache_file.flush()?;
+    }
+
+    Ok(())
+  }
+}
--- crates/mdbook-aquascope/src/main.rs
+++ crates/mdbook-aquascope/src/main.rs
@@ -6,9 +6,11 @@ use std::{
   fs,
   path::{Path, PathBuf},
   process::{Command, Stdio},
+  sync::RwLock,
 };
 
 use anyhow::{bail, Result};
+use mdbook_aquascope::cache::Cache;
 use mdbook_preprocessor_utils::{
   mdbook::preprocess::PreprocessorContext, Asset, SimplePreprocessor,
 };
@@ -33,17 +35,11 @@ const FRONTEND_ASSETS: [Asset; 2] =
 struct AquascopePreprocessor {
   miri_sysroot: PathBuf,
   target_libdir: PathBuf,
+  cache: RwLock<Cache<(String, String), String>>,
 }
 
 impl AquascopePreprocessor {
-  fn process_code(
-    &self,
-    AquascopeBlock {
-      operation,
-      config,
-      code,
-    }: AquascopeBlock,
-  ) -> Result<String> {
+  fn run_aquascope(&self, code: &str, operation: &str) -> Result<String> {
     let tempdir = tempdir()?;
     let root = tempdir.path();
     let status = Command::new("cargo")
@@ -56,11 +52,10 @@ impl AquascopePreprocessor {
       bail!("Cargo failed");
     }
 
-    let (cleaned, annot) = mdbook_aquascope::parse_annotations(&code)?;
-    fs::write(root.join("example/src/main.rs"), &*cleaned)?;
+    fs::write(root.join("example/src/main.rs"), code)?;
 
     let output = Command::new("cargo")
-      .args(["aquascope", &operation])
+      .args(["aquascope", operation])
       .env("SYSROOT", &self.miri_sysroot)
       .env("DYLD_LIBRARY_PATH", &self.target_libdir)
       .env("RUST_BACKTRACE", "1")
@@ -68,7 +63,7 @@ impl AquascopePreprocessor {
       .output()?;
     if !output.status.success() {
       let error = String::from_utf8(output.stderr)?;
-      bail!("Aquascope failed for program:\n{cleaned}\nwith error:\n{error}")
+      bail!("Aquascope failed for program:\n{code}\nwith error:\n{error}")
     }
 
     let response = String::from_utf8(output.stdout)?;
@@ -76,10 +71,38 @@ impl AquascopePreprocessor {
     if let Some(err) = response_json.get("Err") {
       let stderr = String::from_utf8(output.stderr)?;
       bail!(
-        "Aquascope failed for program:\n{cleaned}\nwith error: {}\n{stderr}",
+        "Aquascope failed for program:\n{code}\nwith error: {}\n{stderr}",
         err.as_str().unwrap()
       )
     }
+
+    Ok(response)
+  }
+
+  fn process_code(
+    &self,
+    AquascopeBlock {
+      operation,
+      config,
+      code,
+    }: AquascopeBlock,
+  ) -> Result<String> {
+    let (cleaned, annot) =
+      mdbook_aquascope::annotations::parse_annotations(&code)?;
+
+    let key = (cleaned.clone(), operation.clone());
+    let cached_response = {
+      let cache = self.cache.read().unwrap();
+      cache.get(&key).cloned()
+    };
+    let response = match cached_response {
+      Some(response) => response,
+      None => {
+        let response = self.run_aquascope(&cleaned, &operation)?;
+        self.cache.write().unwrap().set(key, response.clone());
+        response
+      }
+    };
 
     let mut html = String::from(r#"<div class="aquascope-embed""#);
 
@@ -174,9 +197,12 @@ impl SimplePreprocessor for AquascopePreprocessor {
     )?;
     let target_libdir = PathBuf::from(output);
 
+    let cache = RwLock::new(Cache::load()?);
+
     Ok(AquascopePreprocessor {
       miri_sysroot,
       target_libdir,
+      cache,
     })
   }
 
@@ -220,8 +246,61 @@ impl SimplePreprocessor for AquascopePreprocessor {
   fn all_assets(&self) -> Vec<Asset> {
     self.linked_assets()
   }
+
+  fn finish(self) {
+    self.cache.write().unwrap().save().unwrap();
+  }
 }
 
 fn main() {
   mdbook_preprocessor_utils::main::<AquascopePreprocessor>()
+}
+
+#[cfg(test)]
+mod test {
+  use std::fs;
+
+  use anyhow::Result;
+  use mdbook_aquascope::cache::CACHE_PATH;
+  use mdbook_preprocessor_utils::testing::MdbookTestHarness;
+
+  use super::AquascopePreprocessor;
+
+  #[test]
+  fn cache_test() -> Result<()> {
+    let harness = MdbookTestHarness::new()?;
+    let mk_contents = |x: &str| {
+      format!(
+        r#"
+```aquascope,interpreter
+fn main() {{
+  let x = {x};
+}}
+```
+    "#
+      )
+    };
+    let chapter_path = harness.root().join("src/chapter_1.md");
+    fs::write(&chapter_path, mk_contents("0"))?;
+
+    let _book =
+      harness.compile::<AquascopePreprocessor>(serde_json::json!({}))?;
+
+    // After running the first compile, a cache should exist
+    let cache_path = harness.root().join(CACHE_PATH);
+    let cache_contents = fs::read(&cache_path)?;
+
+    // After a second compile, the cache should be unchanged
+    let _book =
+      harness.compile::<AquascopePreprocessor>(serde_json::json!({}))?;
+    assert_eq!(fs::read(&cache_path)?, cache_contents);
+
+    // After modifying the chapter and recompiling, the cache should be changed
+    fs::write(&chapter_path, mk_contents("1"))?;
+    let _book =
+      harness.compile::<AquascopePreprocessor>(serde_json::json!({}))?;
+    assert_ne!(fs::read(&cache_path)?, cache_contents);
+
+    Ok(())
+  }
 }
