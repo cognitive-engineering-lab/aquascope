8c044f483b95ba178f51f050ffc0eb69b14728f1
Gavin Gray <gavinleroy6@gmail.com>
2023-02-02 17:23:33 +0100

    trying to fix puppeteer tests from flaking

--- frontend/packages/aquascope-standalone/jest.config.cjs
+++ frontend/packages/aquascope-standalone/jest.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  // this preset is necessary to import ESM-format JS files
-  // from the current module's dist directory
-  preset: "ts-jest/presets/js-with-ts-esm",
+
+const merge = require('merge')
+const { jsWithTsESM: tsjPreset } = require('ts-jest/presets')
+const puppeteerPreset = require('jest-puppeteer/jest-preset')
+
+module.exports = merge.recursive(tsjPreset, puppeteerPreset, {
+  // preset: "ts-jest/presets/js-with-ts-esm",
+  // preset: "jest-puppeteer",
+  globals: {
+    URL: "<http://localhost:8000>"
+  },
   testEnvironment: "node",
+  verbose: true,
   maxWorkers: 2,
   roots: ['<rootDir>/tests']
-};
+});
--- frontend/packages/aquascope-standalone/tests/integration/standalone.test.ts
+++ frontend/packages/aquascope-standalone/tests/integration/standalone.test.ts
@@ -11,8 +11,28 @@ describe("Aquascope Standalone", () => {
   let page: Page;
 
   beforeAll(async () => {
+    // NOTE: here's a good way to debug a puppeteer test.
+    // browser = await puppeteer.launch({
+    //   headless: false,
+    //   product: "chrome",
+    //   args: ["--start-maximized"],
+    //   defaultViewport: { width: 1700, height: 800 },
+    //   slowMo: 250,
+    // });
     browser = await puppeteer.launch();
+    // there seems to be a discrepency between headless and headed modes.
+    // See: https://github.com/puppeteer/puppeteer/issues/665
+    //
+    // In a headless mode the browser seems to skip rendering visual
+    // elements (just a gut feeling). Setting the language and
+    // user agent seem to help keep things consistent.
     page = await browser.newPage();
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    });
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+    );
   });
 
   afterAll(async () => {
@@ -21,18 +41,13 @@ describe("Aquascope Standalone", () => {
 
   beforeEach(async () => {
     await page.goto(`http://localhost:8000`, {
-      waitUntil: "domcontentloaded",
+      waitUntil: "networkidle0",
     });
   });
 
   it("runs permissions", async () => {
     await page.waitForSelector("#showPermissions");
     await page.click("#showPermissions");
-    await page.waitForNetworkIdle();
-
-    let crashedElement = await page.$(".aquascope-crash");
-    // No crashed elements
-    expect(crashedElement).toBeNull();
 
     await page.waitForSelector(permStackSelector);
     let stackWidgets = await page.$$(permStackSelector);
@@ -41,6 +56,10 @@ describe("Aquascope Standalone", () => {
     await page.waitForSelector(permStepSelector);
     let stepWidgets = await page.$$(permStepSelector);
     expect(stepWidgets.length).toBeGreaterThan(0);
+
+    let crashedElement = await page.$(".aquascope-crash");
+    // No crashed elements
+    expect(crashedElement).toBeNull();
   });
 
   // it("runs the interpreter", async () => {
