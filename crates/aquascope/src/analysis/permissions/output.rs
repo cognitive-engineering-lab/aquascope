d379ee0b1551a8cfb6a3e5479ae72bf7b84c1d4f
Gavin Gray <gavinleroy6@gmail.com>
2023-02-06 21:46:35 +0100

    fix typos

--- crates/aquascope/src/analysis/permissions/output.rs
+++ crates/aquascope/src/analysis/permissions/output.rs
@@ -56,7 +56,7 @@ where
   /// .decl cannot_read(Path:path, Point:point)
   ///
   /// cannot_read(Path, Loan, Point) :-
-  ///    path_moved_at(Path, Point);
+  ///    path_maybe_uninitialized_on_entry(Path, Point);
   ///    loan_conflicts_with(Loan, Path),
   ///    loan_live_at(Loan, Point),
   ///    loan_mutable(Loan).
@@ -68,7 +68,7 @@ where
   /// .decl cannot_write(Path:path, Point:point)
   ///
   /// cannot_write(Path, Loan, Point) :-
-  ///    path_moved_at(Path, Point);
+  ///    path_maybe_uninitialized_on_entry(Path, Point);
   ///    loan_conflicts_with(Loan, Path),
   ///    loan_live_at(Loan, Point).
   ///
@@ -79,7 +79,7 @@ where
   /// .decl cannot_drop(Path, Loan, Point)
   ///
   /// cannot_drop(Path, Loan, Point)
-  ///    path_moved_at(Path, Point);
+  ///    path_maybe_uninitialized_on_entry(Path, Point);
   ///    loan_conflicts_with(Loan, Path),
   ///    loan_live_at(Loan, Point).
   ///
