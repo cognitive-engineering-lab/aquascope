597e1c2c12a37d3109ed2df2b0b3c0267ef31c46
Will Crichton <wcrichto@cs.stanford.edu>
2023-03-08 13:44:13 -0800

    Provide abstract visualization for iterators. Add line numbers to logging

--- crates/aquascope_front/src/bin/aquascope-driver.rs
+++ crates/aquascope_front/src/bin/aquascope-driver.rs
@@ -1,4 +1,24 @@
+use std::{ffi::OsStr, io::Write, path::Path};
+
 fn main() {
-  env_logger::init();
+  env_logger::Builder::from_default_env()
+    .format(|buf, record| {
+      let file = record.file().unwrap_or("unknown");
+      let file = Path::new(file);
+      let components = file.iter().collect::<Vec<_>>();
+      let file_short =
+        components[components.len() - 2 ..].join(OsStr::new("/"));
+      writeln!(
+        buf,
+        "[{} {}:{} {}] - {}",
+        record.level(),
+        file_short.to_string_lossy(),
+        record.line().unwrap_or(0),
+        buf.timestamp(),
+        record.args()
+      )
+    })
+    .init();
+
   rustc_plugin::driver_main(aquascope_front::AquascopePlugin);
 }
