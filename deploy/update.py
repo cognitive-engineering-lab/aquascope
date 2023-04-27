9921135dc310db4383c8c8cbfc8c0d8e6a085072
Gavin Gray <gavinleroy6@gmail.com>
2023-04-27 16:36:23 +0200

    Inspect JSON response.

--- deploy/update.py
+++ deploy/update.py
@@ -10,36 +10,36 @@ with open('generated.sh', 'w') as fout:
 
 set -euv -o pipefail
 
-root=/home/$USER
-artifact_dir=$root/artifacts
-binary_path=$artifact_dir/aquascope_serve
-
-owner="cognitive-engineering-lab"
-repo="aquascope"
+ROOT=/home/$USER
+ARTIFACT_DIR=$ROOT/artifacts
+BINARY_PATH=$ARTIFACT_DIR/aquascope_serve
+OWNER="cognitive-engineering-lab"
+REPO="aquascope"
 
 # Do all work from home directory
-cd $root
+cd $ROOT
 
 # Get the binary's hash so we know if it has changed
 previous_binary_hash=""
-if [[ -f "${{binary_path}}" ]]; then
-    previous_binary_hash=$(md5sum "${{binary_path}}")
+if [[ -f "${{BINARY_PATH}}" ]]; then
+    previous_binary_hash=$(md5sum "${{BINARY_PATH}}")
 fi
 
 # Get the latest workflow run ID
 curl -L \
         -H "Authorization: Bearer {TK}" \
-        https://api.github.com/repos/$owner/$repo/actions/workflows/pre-release.yml/runs?status=success&branch=main&event=pull_request | \
+        https://api.github.com/repos/$OWNER/$REPO/actions/workflows/pre-release.yml/runs?status=success&branch=main&event=pull_request | \
     jq -r '.workflow_runs[0].id' | \
     xargs -I ID curl -L \
         -H "Authorization: Bearer {TK}" \
-        https://api.github.com/repos/$owner/$repo/actions/runs/ID/artifacts | \
-    jq -r '.artifacts[] | \
-    select(.name == "server-artifacts") | \
-    .archive_download_url' | \
-    xargs -I URL curl -L \
-        -H "Authorization: Bearer {TK}" \
-        URL -o artifacts.zip
+        https://api.github.com/repos/$OWNER/$REPO/actions/runs/ID/artifacts
+    # | \
+    # jq -r '.artifacts[] | \
+    # select(.name == "server-artifacts") | \
+    # .archive_download_url' | \
+    # xargs -I URL curl -L \
+    #     -H "Authorization: Bearer {TK}" \
+    #     URL -o artifacts.zip
 
 ls -R
 echo SUCCESS
