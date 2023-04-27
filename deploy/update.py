ef8787070897413763e96d44ccf124cc73adb5ed
Gavin Gray <gavinleroy6@gmail.com>
2023-04-27 16:55:53 +0200

    Finalizing script.

--- deploy/update.py
+++ deploy/update.py
@@ -5,8 +5,10 @@ import os
 TK = os.getenv('NAME')
 
 with open('generated.sh', 'w') as fout:
-    fout.write(f'''
-#!/bin/bash
+    fout.write(f'''#!/bin/bash
+
+# NOTE: this file inspired from the Rust Playground
+# https://github.com/rust-lang/rust-playground/blob/main/deployment/update.sh
 
 set -euv -o pipefail
 
@@ -20,9 +22,9 @@ REPO="aquascope"
 cd $ROOT
 
 # Get the binary's hash so we know if it has changed
-previous_binary_hash=""
+PREVIOUS_BINARY_HASH=""
 if [[ -f "${{BINARY_PATH}}" ]]; then
-    previous_binary_hash=$(md5sum "${{BINARY_PATH}}")
+    PREVIOUS_BINARY_HASH=$(md5sum "${{BINARY_PATH}}")
 fi
 
 # Get the latest workflow run ID
@@ -32,63 +34,20 @@ curl -L \
     jq -r '.workflow_runs[0].id' | \
     xargs -I ID curl -L \
         -H "Authorization: Bearer {TK}" \
-        https://api.github.com/repos/$OWNER/$REPO/actions/runs/ID/artifacts > \
-    response.txt
-    # | \
-    # jq -r '.artifacts[] | select(.name == "server-artifacts") | .archive_download_url' | \
-    # xargs -I URL curl -L \
-    #     -H "Authorization: Bearer {TK}" \
-    #     URL -o artifacts.zip
+        https://api.github.com/repos/$OWNER/$REPO/actions/runs/ID/artifacts | \
+    jq -r '.artifacts[] | select(.name == "server-artifacts") | .archive_download_url' | \
+    xargs -I URL curl -L \
+        -H "Authorization: Bearer {TK}" \
+        URL -o artifacts.zip
 
-cat response.txt
+# Extract the artifacts
+unzip artifacts.zip -d $ARTIFACT_DIR
+docker load -i $ARTIFACT_DIR/image.tar
+chmod +x "${{BINARY_PATH}}"
 
-echo SUCCESS
+# Restart to get new server binary
+if [[ -z "${{PREVIOUS_BINARY_HASH}}" ]] || ! md5sum -c <(echo "${{PREVIOUS_BINARY_HASH}}") --status; then
+    sudo service playground stop || true
+    sudo service playground start
+fi
     ''')
-
-#     fout.write(f'''
-# #!/bin/bash
-
-# set -euv -o pipefail
-
-# root=/home/$USER
-# artifact_dir=$root/artifacts
-# binary_path=$artifact_dir/aquascope_serve
-
-# owner="cognitive-engineering-lab"
-# repo="aquascope"
-
-# # Do all work from home directory
-# cd $root
-
-# # Clean old docker images
-# docker system prune -f || true
-
-# # Get the binary's hash so we know if it has changed
-# previous_binary_hash=""
-# if [[ -f "${{binary_path}}" ]]; then
-#     previous_binary_hash=$(md5sum "${{binary_path}}")
-# fi
-
-# # Get the latest workflow run ID
-# run_id=$(curl -s -H "Authorization: Bearer {TK}" \
-#   "https://api.github.com/repos/$owner/$repo/actions/workflows/workflow.yml/runs?status=success&branch=main&event=pull_request" \
-#   | jq -r '.workflow_runs[0].id')
-
-# # Download the artifacts for the latest workflow run
-# curl -s -H "Authorization: Bearer {TK}" \
-#   "https://api.github.com/repos/$owner/$repo/actions/runs/$run_id/artifacts" \
-#   | jq -r '.artifacts[] | select(.name == "server-artifacts") | .archive_download_url' \
-#   | xargs curl -sL -o artifacts.zip
-
-# # Extract the artifacts
-# unzip artifacts.zip -d $artifact_dir
-# docker load -i $artifact_dir/image.tar
-
-# chmod +x "${{binary_path}}"
-
-# # Restart to get new server binary
-# if [[ -z "${{previous_binary_hash}}" ]] || ! md5sum -c <(echo "${{previous_binary_hash}}") --status; then
-#     sudo service playground stop || true
-#     sudo service playground start
-# fi
-#     ''')
