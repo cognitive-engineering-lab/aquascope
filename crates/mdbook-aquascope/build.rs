b288292068fdd733f6b20993fd36d5752697829c
Will Crichton <wcrichto@cs.stanford.edu>
2024-09-28 19:01:32 -0400

    Update to mdbook-preprocessor-utils 0.2.0

--- crates/mdbook-aquascope/build.rs
+++ crates/mdbook-aquascope/build.rs
@@ -1,28 +1,8 @@
-use std::{fs, io::ErrorKind, path::Path};
-
-use anyhow::{Error, Result};
+use anyhow::Result;
 
 const SRC_DIR: &str = "../../frontend/packages/aquascope-embed/dist/";
 const DST_DIR: &str = "./js";
 
 fn main() -> Result<()> {
-  println!("cargo:rerun-if-changed={SRC_DIR}");
-
-  let dst_dir = Path::new(DST_DIR);
-  fs::create_dir_all(dst_dir)?;
-
-  let src_entries = match fs::read_dir(SRC_DIR) {
-    Ok(src_entries) => src_entries,
-    Err(err) => match err.kind() {
-      ErrorKind::NotFound => return Ok(()),
-      _ => return Err(Error::new(err)),
-    },
-  };
-
-  for entry in src_entries {
-    let path = entry?.path();
-    fs::copy(&path, dst_dir.join(path.file_name().unwrap()))?;
-  }
-
-  Ok(())
+  mdbook_preprocessor_utils::copy_assets(SRC_DIR, DST_DIR)
 }
